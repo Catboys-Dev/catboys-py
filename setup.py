0d4be281212d0d51cfb3a7bdb81505d473591b99
Kristian Kramer <kristian@beyondcoin.io>
2021-08-08 15:58:13 -0600

    Add README.md to package description

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ import re
 
 from setuptools import setup
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 version = ''
 with open('catboys/__init__.py') as f:
@@ -26,6 +30,8 @@ setup(
     packages=['catboys'],
     license='GNU v3',
     description='A Python module that uses the Catboys API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=requirements
 )
